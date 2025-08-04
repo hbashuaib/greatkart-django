ca56a8b7366b7bdcacf44a2aa840a5df241d9a66
Hussein Bashuaib <husseinbashuaib@gmail.com>
2025-08-04 20:18:05 +0300

    django authentication

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -134,3 +134,19 @@ STATICFILES_DIRS = [
 # Media Files Settings:
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR /'media'
+
+from django.contrib.messages import constants as messages
+
+MESSAGE_TAGS = {
+    messages.ERROR: "danger",
+    50: "critical",
+}
+
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
+
+# SMTP Configuration
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'husseinbashuaib@gmail.com'
+EMAIL_HOST_PASSWORD = 'rdup sels zjej iutm'
+EMAIL_USE_TLS = True
