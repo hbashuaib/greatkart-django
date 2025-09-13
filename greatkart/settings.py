a5edee83f09a5b64134ba9dc7512b4f5812b1cfd
Hussein Bashuaib <husseinbashuaib@gmail.com>
2025-09-13 20:39:31 +0300

    last update allowed host modified-AWS Environment OK

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = ['django-greatkart-env.eba-upimfmcx.us-west-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['django-greatkart-env.eba-zrkidhcx.us-west-2.elasticbeanstalk.com']
 
 
 # Application definition
