e09bc93be0500e5bbf425e33080c7158424349ea
Hussein Bashuaib <husseinbashuaib@gmail.com>
2025-09-20 19:32:26 +0300

    new updates

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -132,7 +132,8 @@ STORAGES = {
 # Set the default file storage for media files
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-
+# Static files storage
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 
 # Password validation
@@ -171,6 +172,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_LOCATION = 'static'
 STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
 MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
 
