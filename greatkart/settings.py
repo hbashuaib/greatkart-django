52b44b2c294f57ed5acca2b6f3b880f04c1168b4
Hussein Bashuaib <husseinbashuaib@gmail.com>
2025-09-20 16:04:53 +0300

    new updates

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -92,24 +92,17 @@ AUTH_USER_MODEL = 'accounts.Account'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-if 'RDS_DB_NAME' in os.environ:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': os.environ['RDS_DB_NAME'],
-            'USER': os.environ['RDS_USERNAME'],
-            'PASSWORD': os.environ['RDS_PASSWORD'],
-            'HOST': os.environ['RDS_HOSTNAME'],
-            'PORT': os.environ['RDS_PORT'],
-        }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'greatkartdb',
+        'USER': 'greatkart_admin',
+        'PASSWORD': 'Ghaith27GreatKart',
+        'HOST': 'greatkart-db.cj4woia409ue.us-west-2.rds.amazonaws.com',
+        'PORT': '5432',
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+}
+
 
 
 # Password validation
